2638f8b99ed969621c5237c241e2ec8c10a3ba54
David Ledger <davidledger@live.com.au>
2017-07-28 14:37:19 +1000

    Added Multitouch Support.

--- App 112GW/App_112GW/Peripherals/Touch.cs
+++ App 112GW/App_112GW/Peripherals/Touch.cs
@@ -11,6 +11,8 @@ using Xamarin.Forms.PlatformConfiguration;
 using Xamarin.Forms.Platform;
 using System.Diagnostics;
 
+using System.Collections;
+
 namespace rMultiplatform
 {
     public class TouchPoint
@@ -26,14 +28,14 @@ namespace rMultiplatform
         public Point            Position;
         public eTouchType       TouchType
         {
-            private set;
+            set;
             get;
         }
 
         public TouchPoint(Point pInput, eTouchType pTouchType)
         {
-            Position = pInput;
-            TouchType = pTouchType;
+            Position    = pInput;
+            TouchType   = pTouchType;
         }
     }
     public static class TouchPointFactory
@@ -73,6 +75,129 @@ namespace rMultiplatform
             private set; get;
         }
     }
+
+    public class TouchPinch
+    {
+        private Point _PointA, _PointB;
+        public Point PointA
+        {
+            get
+            {
+                return _PointA;
+            }
+        }
+        public Point PointB
+        {
+            get
+            {
+                return _PointB;
+            }
+        }
+
+        public double Angle
+        {
+            get;
+            private set;
+        }
+        public double Distance
+        {
+            get;
+            private set;
+        }
+
+        double DistanceDelta;
+        double AngleDelta;
+
+        public bool Set(Point A, Point B)
+        {
+            var x1 = A.X;
+            var y1 = A.Y;
+            var x2 = B.X;
+            var y2 = B.Y;
+
+            var deltax = x2 - x1;
+            var deltay = y2 - y1;
+            var distance = Math.Sqrt(deltax * deltax + deltay * deltay);
+            var angle = Math.Atan(deltay / deltax);
+
+            if (Distance == 0)
+            {
+                Distance = distance;
+                Angle = angle;
+            }
+            else
+            {
+                DistanceDelta = distance - Distance;
+                Distance = distance;
+                AngleDelta = angle - Angle;
+                Angle = angle;
+
+                if (Distance > 0)
+                    return true;
+            }
+            return false;
+        }
+        public void Clear()
+        {
+            Distance = 0;
+        }
+
+
+
+        public TouchPinch ()
+        {}
+    }
+
+    public class TouchCursor
+    {
+        private TouchPoint Point;
+        
+        public Point Position
+        {
+            get
+            {
+                return Point.Position;
+            }
+            set
+            {
+                var NewVal = value;
+                var OldVal = Point.Position;
+                var DeltaX = NewVal.X - OldVal.X;
+                var DeltaY = NewVal.Y - OldVal.Y;
+                _Delta = Math.Sqrt(DeltaX * DeltaX + DeltaY * DeltaY);
+                Point.Position = value;
+            }
+        }
+        public TouchPoint.eTouchType TouchType
+        {
+            get
+            {
+                return Point.TouchType;
+            }
+            set
+            {
+                Point.TouchType = value;
+            }
+        }
+        private double _Delta;
+        public double Delta
+        {
+            get
+            {
+                return _Delta;   
+            }
+            set
+            {
+                _Delta = value;
+            }
+        }
+        
+        public TouchCursor (TouchPoint pPoint)
+        {
+            Point = pPoint;
+        }
+    }
+
     public class Touch : RoutingEffect
     {
         public delegate void    TouchActionEventHandler(object sender, TouchActionEventArgs args);
@@ -81,9 +206,56 @@ namespace rMultiplatform
         public event            TouchActionEventHandler     Hover;
         public event            TouchActionEventHandler     PressedMoved;
 
+        public event TouchActionEventHandler Pinch;
+        public event TouchActionEventHandler Swipe;
+
+        public double MultitouchThreshold = 5;
         public bool             Capture { set; get; }
 
-        
+        Dictionary<uint, TouchCursor> Cursors;
+        TouchPinch PinchProcessor = new TouchPinch();
+
+        //Adds a cursor to the map if it doesn't already exist
+        void ProcessCursor(TouchActionEventArgs args)
+        {
+            if (!Cursors.ContainsKey(args.ID))
+                Cursors.Add(args.ID, new TouchCursor(args.Location));
+            else
+            {
+                switch (Cursors.Count)
+                {
+                    case 1:
+                        var item = Cursors[args.ID];
+                        item.Position = args.Location.Position;
+
+                        Debug.WriteLine("Swipe " + item.Delta.ToString());
+                        if (item.Delta > MultitouchThreshold)
+                        {
+
+                        }
+                        break;
+                    case 2:
+                        if(PinchProcessor.Set(Cursors.ElementAt(0).Value.Position, Cursors.ElementAt(1).Value.Position))
+                        {
+                            Debug.WriteLine("Pinch " + item.Delta.ToString());
+                        }
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        void RemoveCursor(TouchActionEventArgs args)
+        {
+            PinchProcessor.Clear();
+            if (!Cursors.ContainsKey(args.ID))
+                Cursors.Remove(args.ID);
+        }
+
+
+        //Removes a cursor from the map
+
+
         //Triggers a threadsafe event
         private void SafeEvent(TouchActionEventHandler EventFunction, Element element, TouchActionEventArgs args)
         {
@@ -101,14 +273,17 @@ namespace rMultiplatform
         }
         private void SafePressed(Element element, TouchActionEventArgs args)
         {
+            ProcessCursor(args);
             SafeEvent(Pressed, element, args);
         }
         private void SafeReleased(Element element, TouchActionEventArgs args)
         {
+            RemoveCursor(args);
             SafeEvent(Released, element, args);
         }
         private void SafePressedMove(Element element, TouchActionEventArgs args)
         {
+            ProcessCursor(args);
             SafeEvent(PressedMoved, element, args);
         }
 
@@ -149,12 +324,6 @@ namespace rMultiplatform
                 PreviousType = type;
         }
 
-        //Initialise class and base
-        public                  Touch() : base("rMultiplatform.Touch")
-        {
-            PreviousType = TouchPoint.eTouchType.eReleased;
-        }
-
         //Common handlers
         public void RaiseAction(TouchActionEventArgs Action)
         {
@@ -172,6 +341,13 @@ namespace rMultiplatform
         public void PressedHandler(object sender, Point pt, uint ID)
         {
             RaiseAction(new TouchActionEventArgs(TouchPointFactory.Pressed(pt), ID));
+        }
+
+        //Initialise class and base
+        public                  Touch() : base("rMultiplatform.Touch")
+        {
+            PreviousType = TouchPoint.eTouchType.eReleased;
+            Cursors = new Dictionary<uint, TouchCursor>();
         }
     }
 }
